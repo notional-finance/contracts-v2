b48cdd945d7b600fe07a3156018389106c446e17
Jeff Wu <jeffywu@pm.me>
2021-03-15 06:49:02 -0700

    Updated market dates tests

--- tests/common/valuation/test_market_dates.py
+++ tests/common/valuation/test_market_dates.py
@@ -1,12 +1,21 @@
-import brownie
 import pytest
 from brownie.test import given, strategy
-from tests.constants import BASE_CASH_GROUP, SECONDS_IN_DAY, START_TIME
+from tests.constants import CASH_GROUP_PARAMETERS, SECONDS_IN_DAY, START_TIME
+from tests.helpers import get_cash_group_with_max_markets
 
 
 @pytest.fixture(scope="module", autouse=True)
-def cashGroup(MockCashGroup, accounts):
-    return accounts[0].deploy(MockCashGroup)
+def cashGroup(MockCashGroup, MockCToken, cTokenAggregator, accounts):
+    cg = accounts[0].deploy(MockCashGroup)
+
+    ctoken = accounts[0].deploy(MockCToken, 8)
+    # This is the identity rate
+    ctoken.setAnswer(1e18)
+    aggregator = cTokenAggregator.deploy(ctoken.address, {"from": accounts[0]})
+    rateStorage = (aggregator.address, 8)
+
+    cg.setAssetRateMapping(1, rateStorage)
+    return cg
 
 
 @pytest.fixture(autouse=True)
@@ -14,22 +23,18 @@ def isolation(fn_isolation):
     pass
 
 
-def test_invalid_max_market(cashGroup):
-    # Tests that we cant go above the max index
-    with brownie.reverts():
-        cg = list(BASE_CASH_GROUP)
-        cg[1] = 10
-        cashGroup.isValidMaturity(cg, 10000, 1)
-
-
 def test_maturity_before_block_time(cashGroup):
-    cg = list(BASE_CASH_GROUP)
+    cashGroupParameters = list(CASH_GROUP_PARAMETERS)
+    cashGroup.setCashGroup(1, cashGroupParameters)
+    (cg, _) = cashGroup.buildCashGroupView(1)
     isValid = cashGroup.isValidMaturity(cg, START_TIME - 1, START_TIME)
     assert not isValid
 
 
 def test_maturity_non_mod(cashGroup):
-    cg = list(BASE_CASH_GROUP)
+    cashGroupParameters = list(CASH_GROUP_PARAMETERS)
+    cashGroup.setCashGroup(1, cashGroupParameters)
+    (cg, _) = cashGroup.buildCashGroupView(1)
     isValid = cashGroup.isValidMaturity(cg, 1601856000 + (91 * SECONDS_IN_DAY), 1601856000)
     assert not isValid
 
@@ -37,11 +42,13 @@ def test_maturity_non_mod(cashGroup):
 @given(
     quarters=strategy("uint40", min_value=0, max_value=800),
     blockTime=strategy("uint40", min_value=START_TIME),
-    maxMarketIndex=strategy("uint8", min_value=1, max_value=9),
+    maxMarketIndex=strategy("uint8", min_value=2, max_value=9),
 )
 def test_valid_maturity(cashGroup, quarters, blockTime, maxMarketIndex):
-    cg = list(BASE_CASH_GROUP)
-    cg[1] = maxMarketIndex
+    cashGroupParameters = get_cash_group_with_max_markets(maxMarketIndex)
+    cashGroup.setCashGroup(1, cashGroupParameters)
+    (cg, _) = cashGroup.buildCashGroupView(1)
+
     tRef = blockTime - blockTime % (90 * SECONDS_IN_DAY)
     maturity = tRef + quarters * (90 * SECONDS_IN_DAY)
     isValid = cashGroup.isValidMaturity(cg, maturity, blockTime)
@@ -58,13 +65,15 @@ def test_valid_maturity(cashGroup, quarters, blockTime, maxMarketIndex):
 @given(
     days=strategy("uint40", min_value=0, max_value=7500),
     blockTime=strategy("uint40", min_value=START_TIME),
-    maxMarketIndex=strategy("uint8", min_value=1, max_value=9),
+    maxMarketIndex=strategy("uint8", min_value=2, max_value=9),
 )
 def test_bit_number(cashGroup, days, blockTime, maxMarketIndex):
+    cashGroupParameters = get_cash_group_with_max_markets(maxMarketIndex)
+    cashGroup.setCashGroup(1, cashGroupParameters)
+    (cg, _) = cashGroup.buildCashGroupView(1)
+
     tRef = blockTime - blockTime % (90 * SECONDS_IN_DAY)
     maturity = tRef + days * SECONDS_IN_DAY
-    cg = list(BASE_CASH_GROUP)
-    cg[1] = maxMarketIndex
 
     isValid = cashGroup.isValidIdiosyncraticMaturity(cg, maturity, blockTime)
     maxMaturity = tRef + cashGroup.getTradedMarket(maxMarketIndex)
