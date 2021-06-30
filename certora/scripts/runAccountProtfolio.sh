7092f51b5d976184fd457e2657752f7f6ab5d5dd
Nurit Dor <nurit@certora.com>
2021-06-30 18:30:38 +0300

    run scripts

--- certora/scripts/runAccountProtfolio.sh
+++ certora/scripts/runAccountProtfolio.sh
@@ -3,8 +3,8 @@ certoraRun contracts/mocks/certora/AccountPortfolioHarness.sol \
  	--solc solc7.6 \
 	--optimistic_loop \
  	--loop_iter 9 \
-	--settings -smt_bitVectorTheory=true \
+	--settings -smt_bitVectorTheory=true,-smt_hashingScheme=plainInjectivity \
 	--packages_path '/c/Users/nurit/.brownie/packages' \
 	--packages @openzeppelin=/c/Users/nurit/.brownie/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=/c/Users/nurit/.brownie/packages/compound-finance \
- 	--solc_args "['--optimize', '--optimize-runs', '200']"  --staging --msg "AccountProtfolio" 
+ 	--solc_args "['--optimize', '--optimize-runs', '200']"   --staging alex/mark-hash-values --msg "AccountProtfolio" 
 	 
