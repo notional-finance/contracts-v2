630b6622098c6a455934160dbb48d4e1b1d1b301
Shelly Grossman <shelly6.g@gmail.com>
2021-07-25 12:34:36 +0300

    improve with BROWNIE_PATH

--- certora/scripts/runAccountContext.sh
+++ certora/scripts/runAccountContext.sh
@@ -4,7 +4,7 @@ certoraRun contracts/mocks/certora/GetterSetterHarness.sol \
 	--optimistic_loop \
  	--loop_iter 1 \
 	--settings -smt_bitVectorTheory=true,-smt_hashingScheme=plainInjectivity \
-	--packages_path '/c/Users/nurit/.brownie/packages' \
-	--packages @openzeppelin=/c/Users/nurit/.brownie/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=/c/Users/nurit/.brownie/packages/compound-finance \
- 	--solc_args "['--optimize', '--optimize-runs', '200']"  --staging alex/bv-solver-strategy --msg "AccountContext - all rules bv branch"
+	--packages_path ${BROWNIE_PATH}/packages \
+	--packages @openzeppelin=${BROWNIE_PATH}/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=${BROWNIE_PATH}/packages/compound-finance \
+ 	--solc_args "['--optimize', '--optimize-runs', '200']"  --msg "AccountContext - all rules bv branch"
 	 
