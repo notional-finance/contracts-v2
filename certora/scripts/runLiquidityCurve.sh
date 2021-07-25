f5fca21113ad6519ae1fadba4dd09b0aca807390
Shelly Grossman <shelly6.g@gmail.com>
2021-07-25 16:32:47 +0300

    include BROWNIE_PATH

--- certora/scripts/runLiquidityCurve.sh
+++ certora/scripts/runLiquidityCurve.sh
@@ -5,8 +5,8 @@ certoraRun contracts/mocks/certora/LiquidityCurveHarness.sol \
  	--loop_iter 1 \
     --rule $1 \
 	--settings -adaptiveSolverConfig=false,-solver=z3,-smt_hashingScheme=plainInjectivity,-postProcessCounterExamples=true \
-	--packages_path '/Users/shelly/.brownie/packages' \
-	--packages @openzeppelin=/Users/shelly/.brownie/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=/Users/shelly/.brownie/packages/compound-finance \
+	--packages_path ${BROWNIE_PATH}/packages \
+	--packages @openzeppelin=${BROWNIE_PATH}/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=${BROWNIE_PATH}/packages/compound-finance \
  	--solc_args "['--optimize']" --msg "LiquidityCurve: $1 - $2" \
 	--staging
 #   -smt_bitVectorTheory=true,
