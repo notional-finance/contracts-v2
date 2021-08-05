d0926fe0965c1949ae942c72e07422331a0f34eb
Jeff Wu <jeffywu@pm.me>
2021-08-05 06:31:44 -0700

    Adding some more test conditions

--- tests/stateful/test_settlement.py
+++ tests/stateful/test_settlement.py
@@ -1,7 +1,7 @@
 import pytest
 from brownie.network.state import Chain
 from tests.constants import HAS_ASSET_DEBT, HAS_CASH_DEBT, SECONDS_IN_QUARTER
-from tests.helpers import get_balance_trade_action, initialize_environment
+from tests.helpers import get_balance_trade_action, get_tref, initialize_environment
 from tests.stateful.invariants import check_system_invariants
 
 chain = Chain()
@@ -46,13 +46,14 @@ def test_settle_bitmap_to_cash(environment, accounts):
     chain.mine(1, timestamp=blockTime + SECONDS_IN_QUARTER)
     environment.notional.initializeMarkets(currencyId, False)
 
-    environment.notional.batchBalanceAndTradeAction(
+    txn = environment.notional.batchBalanceAndTradeAction(
         accounts[1], [collateral], {"from": accounts[1]}
     )
     portfolio = environment.notional.getAccountPortfolio(accounts[1])
     context = environment.notional.getAccountContext(accounts[1])
     assert len(portfolio) == 0
     assert context[1] == HAS_CASH_DEBT
+    assert context[0] == get_tref(txn.timestamp)
 
     check_system_invariants(environment, accounts)
 
@@ -87,7 +88,7 @@ def test_settle_bitmap_shift_assets(environment, accounts):
     chain.mine(1, timestamp=blockTime + SECONDS_IN_QUARTER)
     environment.notional.initializeMarkets(currencyId, False)
 
-    environment.notional.batchBalanceAndTradeAction(
+    txn = environment.notional.batchBalanceAndTradeAction(
         accounts[1], [collateral], {"from": accounts[1]}
     )
     portfolio = environment.notional.getAccountPortfolio(accounts[1])
@@ -96,5 +97,45 @@ def test_settle_bitmap_shift_assets(environment, accounts):
     assert portfolio[0][3] == -fCashAmount
     assert portfolio[0][1] == markets[1][1]
     assert context[1] == HAS_ASSET_DEBT
+    assert context[0] == get_tref(txn.timestamp)
+
+    check_system_invariants(environment, accounts)
+
+
+def test_settle_array_to_cash(environment, accounts):
+    currencyId = 2
+
+    fCashAmount = 100e8
+    borrowAction = get_balance_trade_action(
+        currencyId,
+        "None",
+        [
+            {
+                "tradeActionType": "Borrow",
+                "marketIndex": 1,
+                "notional": fCashAmount,
+                "maxSlippage": 0,
+            }
+        ],
+        withdrawEntireCashBalance=True,
+        redeemToUnderlying=True,
+    )
+    collateral = get_balance_trade_action(3, "DepositAsset", [], depositActionAmount=50000e8)
+
+    environment.notional.batchBalanceAndTradeAction(
+        accounts[1], [borrowAction, collateral], {"from": accounts[1]}
+    )
+
+    blockTime = chain.time()
+    chain.mine(1, timestamp=blockTime + SECONDS_IN_QUARTER)
+    environment.notional.initializeMarkets(currencyId, False)
+
+    environment.notional.batchBalanceAndTradeAction(
+        accounts[1], [collateral], {"from": accounts[1]}
+    )
+    portfolio = environment.notional.getAccountPortfolio(accounts[1])
+    context = environment.notional.getAccountContext(accounts[1])
+    assert len(portfolio) == 0
+    assert context[1] == HAS_CASH_DEBT
 
     check_system_invariants(environment, accounts)
