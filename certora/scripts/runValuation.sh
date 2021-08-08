80c747ac48a6dc679ffcf1f5c05698553eeb08f1
Shelly Grossman <shellygr151@gmail.com>
2021-08-08 14:27:04 +0300

    change

--- certora/scripts/runValuation.sh
+++ certora/scripts/runValuation.sh
@@ -6,4 +6,4 @@ certoraRun contracts/mocks/certora/ValuationHarness.sol \
 	--settings -smt_bitVectorTheory=true,-smt_hashingScheme=plainInjectivity \
 	--packages_path ${BROWNIE_PATH}/packages \
 	--packages @openzeppelin=${BROWNIE_PATH}/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=${BROWNIE_PATH}/packages/compound-finance \
- 	--solc_args "['--optimize', '--optimize-runs', '200']"  --staging --msg "valuation $1"
+ 	--solc_args "['--optimize', '--optimize-runs', '200']"  --msg "valuation $1" --javaArgs '"-Dverbose.times"' --settings -enableEqualitySaturation=false
