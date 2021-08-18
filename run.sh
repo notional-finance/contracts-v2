e90762d46a351e9ea8f83047b4848ff41c7d63ec
Shelly Grossman <shelly6.g@gmail.com>
2021-08-18 15:32:44 +0300

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-certoraRun.py contracts/external/actions/AccountAction.sol \\n\tcontracts/mocks/certora/BalanceStateHarness.sol \\n\tcontracts/mocks/certora/DummyERC20A.sol \\n \t--verify BalanceStateHarness:certora/asset/BalanceState.spec \\n \t--solc solc7.6 \\n\t--optimistic_loop \\n \t--loop_iter 1 \\n\t--cache BalanceHandlerNotional \\n\t--packages_path ${BROWNIE_PATH}/packages \\n\t--packages @openzeppelin=${BROWNIE_PATH}/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=${BROWNIE_PATH}/packages/compound-finance \\n \t--solc_args "['--optimize']" --msg "BalanceState - $1" --javaArgs '"-Dverbose.times -Dverbose.cache"' --staging --rule integrity_depositAssetToken_old
+certoraRun.py contracts/external/actions/AccountAction.sol contracts/mocks/certora/BalanceStateHarness.sol \
+    contracts/mocks/certora/DummyERC20A.sol \
+    --verify BalanceStateHarness:certora/asset/BalanceState.spec \
+    --solc solc7.6 \
+    --optimistic_loop \
+    --loop_iter 1 \
+    --cache BalanceHandlerNotional \
+    --packages_path ${BROWNIE_PATH}/packages \
+    --packages @openzeppelin=${BROWNIE_PATH}/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=${BROWNIE_PATH}/packages/compound-finance \
+    --solc_args "['--optimize']" --msg "BalanceState - $1" --javaArgs '"-Dverbose.times -Dverbose.cache"' --staging --rule integrity_depositAssetToken_old
