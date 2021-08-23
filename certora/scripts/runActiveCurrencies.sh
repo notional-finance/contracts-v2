f62dc47f67771dd67d151ee1321ad8a6a1af212b
Jeff Wu <jeffywu@pm.me>
2021-08-23 15:38:32 -0700

    Updates to scripts

--- certora/scripts/runActiveCurrencies.sh
+++ certora/scripts/runActiveCurrencies.sh
@@ -4,6 +4,6 @@ certoraRun contracts/mocks/certora/AccountPortfolioHarness.sol \
 	--optimistic_loop \
  	--loop_iter 1 \
 	--settings -smt_bitVectorTheory=true,-smt_hashingScheme=plainInjectivity \
-	--packages_path '/c/Users/nurit/.brownie/packages' \
-	--packages @openzeppelin=/c/Users/nurit/.brownie/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=/c/Users/nurit/.brownie/packages/compound-finance \
+	--packages_path ${BROWNIE_PATH}/packages \
+	--packages @openzeppelin=${BROWNIE_PATH}/packages/OpenZeppelin/openzeppelin-contracts@3.4.0-solc-0.7 compound-finance=${BROWNIE_PATH}/packages/compound-finance \
  	--solc_args "['--optimize', '--optimize-runs', '200']"  --staging  --msg "AccountContext - all rules bv"
