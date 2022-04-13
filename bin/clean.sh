7de41b82e14a28edf4a85766e50ebd3c6e62e48c
Jeff Wu <jeffywu@pm.me>
2022-04-13 05:43:34 -0700

    updating dependencies

---
+++ bin/clean.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+rm -Rf artifacts
+rm -Rf build/contracts
+rm -Rf build/interfaces
+rm -Rf build/tests.json
+rm -Rf venv
+rm -Rf node_modules
+rm -Rf .pytest_cache
---
+++ bin/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+npm install
