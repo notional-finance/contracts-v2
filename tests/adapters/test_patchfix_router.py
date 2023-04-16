5cd7b9f17b035d7a3ca715c8afc7c41e6a954f3d
Jeff Wu <jeff@notional.finance>
2023-04-16 05:21:34 -0700

    adding ability to return ownership to patchfix router

--- tests/adapters/test_patchfix_router.py
+++ tests/adapters/test_patchfix_router.py
@@ -25,3 +25,29 @@ def test_patch_fix_router(environment, MockPatchFix, MockRouter, accounts):
 
     assert environment.proxy.getImplementation() == newRouter.address
     assert environment.notional.owner() == accounts[0]
+
+def test_patch_fix_router_return_ownership(environment, MockPatchFix, MockRouter, accounts):
+    newRouter = MockRouter.deploy({"from": accounts[0]})
+    patchFix = MockPatchFix.deploy(
+        environment.proxy.getImplementation(),
+        newRouter.address, 
+        environment.notional.address, 
+        {"from": accounts[0]}
+    )
+
+    originalOwner = environment.notional.owner()
+
+    # Direct ownership transfer
+    environment.notional.transferOwnership(patchFix.address, True, {"from": accounts[0]})
+
+    # Patchfix is now the owner
+    assert environment.notional.owner() == patchFix.address
+
+    with brownie.reverts():
+        # Only owner allowed to call this method
+        patchFix.returnOwnership({"from": accounts[1]})
+
+    # Ownership is returned back to the previous owner
+    patchFix.returnOwnership({"from": patchFix.OWNER()})
+
+    assert environment.notional.owner() == originalOwner
