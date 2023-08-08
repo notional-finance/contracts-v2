c76d438faa34a6cbddf74b945236ebfc9c030e4d
Jeff Wu <jeff@notional.finance>
2023-08-08 11:58:00 -0700

    fix: test script

--- bin/runTests.sh
+++ bin/runTests.sh
@@ -10,3 +10,4 @@ brownie test tests/stateful/vaults
 brownie test tests/stateful/test_!(settlement).py
 brownie test tests/stateful/test_settlement.py
 brownie test tests/mainnet-fork/test_treasury_action.py --network mainnet-fork
+#export CHAIN_ID=42161; brownie test tests/arbitrum-fork/test_pcash_rebalancing.py --network arbitrum-fork
