dc830ebd549abe587d8c68de3f872b561442d6c1
Jeff Wu <jeff@notional.finance>
2023-08-08 12:29:05 -0700

    updating deployment contracts

---
+++ scripts/deploy_v3.py
@@ -0,0 +1,42 @@
+from brownie import accounts, network
+from scripts.deployers.liq_deployer import LiqDeployer
+from scripts.deployers.notional_deployer import NotionalDeployer
+
+
+def deployNotional(deployer, networkName, dryRun):
+    notional = NotionalDeployer(networkName, deployer, dryRun)
+    notional.deployLibs()
+    notional.deployActions()
+    notional.deployPauseRouter()
+    notional.deployRouter()
+    notional.deployProxy()
+
+
+def deployLiquidator(deployer, networkName):
+    liq = LiqDeployer(networkName, deployer)
+    liq.deployExchange()
+    liq.deployFlashLender()
+    liq.deployFlashLiquidator()
+    liq.deployManualLiquidator(1)
+    liq.deployManualLiquidator(2)
+    liq.deployManualLiquidator(3)
+    liq.deployManualLiquidator(4)
+
+
+def main(dryRun=True):
+    networkName = network.show_active()
+    if networkName in ["mainnet-fork", "mainnet-current"]:
+        networkName = "mainnet"
+    elif networkName in ["arbitrum-fork", "arbitrum-current"]:
+        networkName = "arbitrum-one"
+    deployer = accounts.load(networkName.upper() + "_DEPLOYER")
+
+    if dryRun == "LFG":
+        txt = input("Will execute REAL transactions, are you sure (type 'I am sure'): ")
+        if txt != "I am sure":
+            return
+        else:
+            dryRun = False
+
+    deployNotional(deployer, networkName, dryRun)
+    # deployLiquidator(deployer, networkName, dryRun)
