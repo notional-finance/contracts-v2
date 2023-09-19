a7715e9c6ea5d0928da36bd2f67774f27f57fea3
Jeff Wu <jeff@notional.finance>
2023-09-19 11:11:12 -0700

    fix: update some tests

--- tests/arbitrum-fork/test_migrate_usdc.py
+++ tests/arbitrum-fork/test_migrate_usdc.py
@@ -42,14 +42,18 @@ def test_migrate_usdc(v3env, MigrateUSDC, UnderlyingHoldingsOracle, accounts):
 
     balanceBefore = usdc_e.balanceOf(notional.address)
     notional.transferOwnership(migrate, False, {"from": notional.owner()})
+    lastUnderlyingBefore = notional.getPrimeFactors(3, chain.time())['factors']['lastTotalUnderlyingValue']
     txn = migrate.atomicPatchAndUpgrade({"from": notional.owner()})
 
     notional.accruePrimeInterest(3, {"from": accounts[0]})
+    lastUnderlyingAfter = notional.getPrimeFactors(3, chain.time() + 1)['factors']['lastTotalUnderlyingValue']
 
+    assert lastUnderlyingBefore == lastUnderlyingAfter
     assert proxy.getImplementation() == routerBefore
     assert notional.getCurrency(3)['underlyingToken'][0] == usdc.address
     assert balanceBefore == usdc.balanceOf(notional.address)
     assert usdc_e.balanceOf(notional.address) == 0
+    assert usdc_e.balanceOf(funding) == balanceBefore
 
     usdc.transfer(funding, 100e6, {"from": nativeUSDCWhale})
     notional.depositUnderlyingToken(funding, 3, 100e6, {"from": funding})
