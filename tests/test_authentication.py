7b042c3f3560b28f761805ef371c4b38339c2767
Jeff Wu <jeff@notional.finance>
2024-01-21 06:37:31 -0800

    fix: remove auth test

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -114,9 +114,6 @@ def test_non_callable_methods(environment, accounts):
         )
 
     with brownie.reverts("Unauthorized caller"):
-        environment.notional.nTokenRedeem(
-            accounts[2], 1, 100e8, False, False, {"from": accounts[1]}
-        )
         environment.notional.batchBalanceAction(accounts[2], [], {"from": accounts[1]})
         environment.notional.batchBalanceAndTradeAction(accounts[2], [], {"from": accounts[1]})
 
